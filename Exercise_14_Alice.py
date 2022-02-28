feedc867aff49e513f0e132504c7d8c466ec552f
Alice Caffyn <alice.caffyn@gmail.com>
2022-02-28 19:48:33 +0000

    Alice code updated post session with Victoria

--- Exercise_14_Alice.py
+++ Exercise_14_Alice.py
@@ -1,7 +1,4 @@
-# defined functions
-# ATTEMPT at using  keyword parameters
-# def convert_user_input (**rps):
-#     argsdict = dict(R='Rock', P='Paper', S='Scissors')
+# updated post Monday session with Victoria
 
 
 # creating function to convert user input - 'R', 'P' or 'S' - into 'Rock', 'Paper' or 'Scissors'
@@ -13,40 +10,44 @@ def convert_user_input(user_input_letter):
     else:
         user_selection = 'Scissors'
     return user_selection
+# is it not a problem that this function uses variable names?
 
 
-# creating a function to generate a random number between 0 and 2
-def generate_random_number_0_to_2():
+# creating a function to generate a random number between 0 and 2 and convert it to 'Rock', 'Paper', or 'Scissors'
+def generate_computer_selection():
     import random
+    options = ['Rock', 'Paper', 'Scissors']
     random_number = random.randint(0, 2)
-    return random_number
+    return options[random_number]
 
 
+# removed this function, as combined with above
 # creating function to convert computer number - 0, 1, or 2 - into 'Rock', 'Paper' or 'Scissors'
-def convert_computer_number(computer):
-    if computer_number == 0:
-        computer_selection = 'Rock'
-    elif computer_number == 1:
-        computer_selection = 'Paper'
-    else:
-        computer_selection = 'Scissors'
-    return computer_selection
+# def convert_computer_number(computer_choice):
+#     if computer_choice == 0:
+#         computer_selection = 'Rock'
+#     elif computer_choice == 1:
+#         computer_selection = 'Paper'
+#     else:
+#         computer_selection = 'Scissors'
+#     return computer_selection
 
 
 # creating a function to determine win, loss or draw:
+# grouped all win and lose situations together for ease of reading
 def determine_game_outcome(user, computer):
-    if converted_user_input == converted_computer_word:
+    if user == computer:
         outcome = 'D'
-    elif converted_user_input == 'Rock' and converted_computer_word == 'Scissors':
+    elif user == 'Rock' and computer == 'Scissors':
         outcome = 'W'
-    elif converted_user_input == 'Rock' and converted_computer_word == 'Paper':
+    elif user == 'Paper' and computer == 'Rock':
+        outcome = 'W'
+    elif user == 'Scissors' and computer == 'Paper':
+        outcome = 'W'
+    elif user == 'Rock' and computer == 'Paper':
         outcome = 'L'
-    elif converted_user_input == 'Paper' and converted_computer_word == 'Rock':
-        outcome = 'W'
-    elif converted_user_input == 'Paper' and converted_computer_word == 'Scissors':
+    elif user == 'Paper' and computer == 'Scissors':
         outcome = 'L'
-    elif converted_user_input == 'Scissors' and converted_computer_word == 'Paper':
-        outcome = 'W'
     else:
         outcome = 'L'
     return outcome
@@ -55,29 +56,22 @@ def determine_game_outcome(user, computer):
 # STEP 1 - user input: R, P, or S
 accepted_user_inputs = ['R', 'P', 'S']
 
-# REVISIT: couldn't figure out how to use upper method to ensure entry is not case-sensitive
-# REVISIT: can't get this while loop to work without printing error message
-
 user_input = None
-
 while user_input not in accepted_user_inputs:
-    user_input = input('Please enter R, P, or S: ')
+    user_input = input('Please enter R, P, or S: ').upper()
+    # REVISIT: can't get this while loop to work without printing error message
     # print('You have not entered an accepted letter, please try again.')
 
 # STEP 2 - convert user input to 'Rock', 'Paper' or 'Scissors'
-converted_user_input = convert_user_input(user_input)
-print('You have selected: ', converted_user_input)
+user = convert_user_input(user_input)
+print('You have selected: ', user)
 
-# STEP 3 - generate random number between 0 and 2:
-computer_number = generate_random_number_0_to_2()
-
-# STEP 4 - convert computer number to string to compare to user selection:
-converted_computer_word = convert_computer_number(computer_number)
-print('Computer has selected: ', converted_computer_word)
+# STEP 3 - generate computer selection:
+computer_choice = generate_computer_selection()
+print('Computer has selected: ', computer_choice)
 
 # STEP 4 - compare user input and computer number to determine outcome of game:
-
-game_outcome = determine_game_outcome(converted_user_input, converted_computer_word)
+game_outcome = determine_game_outcome(user, computer_choice)
 
 win_message = "You win!"
 lose_message = "You lose!"
@@ -88,9 +82,7 @@ if game_outcome == 'W':
 elif game_outcome == 'L':
     print(lose_message)
 else:
-    print(draw_message)
-
-# thoughts: could you write a shorter script which compares the user and computer selections as integers rather than converting the random number into string?
+    print(draw_message)cd 
 
 
 
