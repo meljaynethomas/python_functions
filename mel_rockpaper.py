bbda16018c70ff36b5671f2f035df637c0c908f3
Mel <meljaynethomas@gmail.com>
2022-03-03 14:55:55 +0000

    refactored code and got while loop working; added option for player to restart game

--- mel_rockpaper.py
+++ mel_rockpaper.py
@@ -1,56 +1,59 @@
-# ROCK - 0
-# PAPER - 1
-# SCISSORS - 2
-
 import random
 
-# create another function to get player input, and another to get computer input
-# can then implement a while loop to keep looping until player inputs correct information....
 
-print("ROCK...PAPER...SCISSORS!")
-print("Can you beat the computer?")
-player = input("Enter R for Rock, P for Paper, and S for Scissors: ")
-if player == "R":
-    player = 0
-    print(f"Your choice is: Rock")
-elif player == "P":
-    player = 1
-    print(f"Your choice is: Paper")
-elif player == "S":
-    player = 2
-    print(f"Your choice is: Scissors")
-else:
-    print("You have entered an incorrect character. Please try again!")
-
-
-computer = random.randint(0, 2)
-if computer == 0:
-    print(f"Computer chooses: Rock")
-if computer == 1:
-    print(f"Computer chooses: Paper")
-if computer == 2:
-    print(f"Computer chooses: Rock")
-
-
-def compare_values(player, computer):
-    if player == 0 and computer == 0:
+def compare_values(player_choice, computer_choice):
+    if player_choice == computer_choice:
         print("It's a draw!")
-    if player == 1 and computer == 1:
-        print("It's a draw!")
-    if player == 2 and computer == 2:
-        print("It's a draw!")
-    if player == 0 and computer == 1:
+    if player_choice == 0 and computer_choice == 1:
         print("Paper Wraps Rock - Computer Wins!")
-    if player == 0 and computer == 2:
+    if player_choice == 0 and computer_choice == 2:
         print("Rock Smashes Scissors - Player Wins!")
-    if player == 1 and computer == 0:
+    if player_choice == 1 and computer_choice == 0:
         print("Paper Wraps Rock - Player Wins!")
-    if player == 1 and computer == 2:
+    if player_choice == 1 and computer_choice == 2:
         print("Scissors Cut Paper - Computer Wins!")
-    if player == 2 and computer == 0:
+    if player_choice == 2 and computer_choice == 0:
         print("Rock Smashes Scissors - Computer Wins!")
-    if player == 2 and computer == 1:
+    if player_choice == 2 and computer_choice == 1:
         print("Scissors Cut Paper - Player Wins!")
 
 
-compare_values(player, computer)
+print("ROCK...PAPER...SCISSORS!")
+print("Can You Beat The Computer?")
+
+while True:
+
+    player_input = ""
+    accepted_player_inputs = ['R', 'P', 'S']
+
+    while player_input not in accepted_player_inputs:
+        player_input = input("Enter R for Rock, P for Paper, and S for Scissors: ").upper()
+
+    if player_input == "R":
+        player_input = 0
+        print(f"Your Choice Is: Rock")
+    elif player_input == "P":
+        player_input = 1
+        print(f"Your Choice Is: Paper")
+    elif player_input == "S":
+        player_input = 2
+        print(f"Your Choice Is: Scissors")
+
+    computer_input = random.randint(0, 2)
+    if computer_input == 0:
+        print(f"Computer Chooses: Rock")
+    elif computer_input == 1:
+        print(f"Computer Chooses: Paper")
+    elif computer_input == 2:
+        print(f"Computer Chooses: Scissors")
+
+    compare_values(player_input, computer_input)
+
+    play_again = input("Would You Like To Play Again? Y/N: ").upper()
+    if play_again == "N":
+        break
+
+
+
+
+
